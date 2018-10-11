4354babf631630d3465afd483932e847325dc38a
ju-zp <jhuntington@tutanota.com>
2018-10-11 15:56:26 +0100

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(anagram_array)
+        new_array = []
+        anagram_array.each do |anagram|
+            if(anagram.split("").sort == self.word.split("").sort)
+                new_array << anagram
+            end
+        end
+        new_array
+    end
+    
+end
