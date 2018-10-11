1ca107f63a25a2915e677409393ba710cc0c7c09
github username <slonanuga@gmail.com>
2018-10-11 19:42:33 +0100

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
-# Your code goes here!
+class Anagram
+
+attr_accessor :word
+
+def initialize(word)
+    @word = word
+
+end
+
+def match(array)
+
+    @word = word.scan(/\w/).sort.join("")
+    word
+    
+    array.select do |anagram|
+        anagram.scan(/\w/).sort.join("") == word
+
+   end
+end
+
+
+
+end
