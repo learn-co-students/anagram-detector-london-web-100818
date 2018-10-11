165ff1f900bb1ae0049f6af834d11e94a11e4c42
intuitive-programmer <steven@intuitiveprogrammer.com>
2018-10-11 20:17:19 +0100

    Done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(original_word)
+    @original_word = original_word
+  end
+
+  def match(selection)
+    selection.keep_if { |word| @original_word.split(//).sort == word.split(//).sort }
+  end
+
+end
