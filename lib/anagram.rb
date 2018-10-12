e55c5cdd81a541e81e55b1ee3b0ed9553ef3ebc3
natashabuck <natashabuck1@gmail.com>
2018-10-12 08:31:31 +0100

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(possible_grams)
+    matches = []
+    possible_grams.each { |p_g|
+       if (p_g.split("")).sort == (@word.split("")).sort
+         matches << p_g
+       end
+    }
+    matches
+
+  end
+
+end
