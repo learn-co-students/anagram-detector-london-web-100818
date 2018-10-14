4a9c1d52d4349cb4207bcda84e87a51e4b420e97
congo-quinine <congo.quinine@gmail.com>
2018-10-14 12:19:08 +0100

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,30 @@
 # Your code goes here!
+
+require 'pry'
+
+class Anagram
+
+  attr_accessor :word
+  attr_reader :match
+
+  @@all = []
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(arr)
+    @word = @word.split('').sort
+
+  new_arr = arr.select do |i|
+      if i.split('').sort == @word
+          i
+        end
+      end
+   return new_arr
+  end
+
+
+# binding.pry
+# puts 'eof'
+end
